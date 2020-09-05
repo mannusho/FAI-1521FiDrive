22fc0da5a31cb36e2d07f3a50ff99a697d919a23
mannusho <aloneinpatagonia@gmail.com>
2020-09-05 18:25:24 -0300

    Cambios: Agregado de Licencia GNU/GLP

---
+++ FAI-1521_FiDrive/Index.php
@@ -0,0 +1,5 @@
+<?php
+echo "Programacion Web Dinamica  ";
+echo "Esta es una aplicación para evaluar mis progresos en la materia PWD 2020.Mis nombres es Hector Manuel Ortiz y mi Nro de Legajo FAI 1521 ";
+
+?>
